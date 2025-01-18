bdd1db1fd0462c31647c434a08f368d13d62bf1b
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-01-18 22:40:19 +0200

    [TMP] netdutils: disable ipv6 [1/2]

--- staticlibs/client-libs/netd/com/android/net/module/util/NetdUtils.java
+++ staticlibs/client-libs/netd/com/android/net/module/util/NetdUtils.java
@@ -161,10 +161,10 @@ public class NetdUtils {
         netd.tetherInterfaceAdd(iface);
         networkAddInterface(netd, iface, maxAttempts, pollingIntervalMs);
         // Activate a route to dest and IPv6 link local.
-        modifyRoute(netd, ModifyOperation.ADD, INetd.LOCAL_NET_ID,
-                new RouteInfo(dest, null, iface, RTN_UNICAST));
-        modifyRoute(netd, ModifyOperation.ADD, INetd.LOCAL_NET_ID,
-                new RouteInfo(new IpPrefix("fe80::/64"), null, iface, RTN_UNICAST));
+        // modifyRoute(netd, ModifyOperation.ADD, INetd.LOCAL_NET_ID,
+        //        new RouteInfo(dest, null, iface, RTN_UNICAST));
+        // modifyRoute(netd, ModifyOperation.ADD, INetd.LOCAL_NET_ID,
+        //        new RouteInfo(new IpPrefix("fe80::/64"), null, iface, RTN_UNICAST));
     }
 
     /**
